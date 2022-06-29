e74e9c84a3c4e7c671431e201a4fbe08367504c3
Tanapat C <63283610+nlcetanapat@users.noreply.github.com>
2022-06-29 20:12:02 +0700

    env

--- pages/inspire_page.vue
+++ pages/inspire_page.vue
@@ -3,7 +3,7 @@
     <v-col class="text-center">
       <div>{{ currentLatLon }}</div>
       <div>Is this mobile breakpoint width: {{ isMobile }}</div>
-      <!-- <div>App Version: {{}}</div> -->
+      <div>App Version: {{ $config.version }}</div>
       <img src="/v.png" alt="Vuetify.js" class="mb-5" />
       <blockquote class="blockquote">
         &#8220;First, solve the problem. Then, write the code.&#8221;
